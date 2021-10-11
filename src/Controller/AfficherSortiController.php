74d1e895fb1e0efea1eba3a438b2c110217a6d77
Ryad86180 <rdwalibi@live.fr>
2021-10-11 14:43:27 +0200

    fonctionnalité recherche

--- src/Controller/AfficherSortiController.php
+++ src/Controller/AfficherSortiController.php
@@ -8,17 +8,36 @@ use App\Entity\Sites;
 use App\Entity\Sorties;
 use App\Repository\ParticipantsRepository;
 use App\Repository\SitesRepository;
+use App\Repository\SortiesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AfficherSortiController extends AbstractController
 {
     /**
-     * @Route("/afficher/sorti", name="afficher_sorti")
+     * @Route("/sortie", name="afficher_sorti")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $nom = $request->request->get('nom');
+
+        if($request->isMethod('POST')){
+            $sorties = $this->getDoctrine()
+            ->getRepository(Sorties::class)
+            ->findByNom($nom);    
+            
+            $participant = $this->getDoctrine()
+            ->getRepository(Participants::class)
+            ->find(1);         
+            
+            $sites = $this->getDoctrine()
+            ->getRepository(Sites::class)
+            ->findAll();            
+
+        } else{
+
         $participant = $this->getDoctrine()
             ->getRepository(Participants::class)
             ->find(1);
@@ -30,6 +49,9 @@ class AfficherSortiController extends AbstractController
         $sites = $this->getDoctrine()
             ->getRepository(Sites::class)
             ->findAll();
+        }
+
+
 
         return $this->render('afficher_sorti/index.html.twig', [
             'controller_name' => 'AfficherSortiController',
